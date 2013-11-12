f27ec6d6a98e2648b7470de624896928e0e4d96e
denizozger <deniz@parallelbrains.com>
2013-11-12 11:50:43 +0000

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+  console.log('Server listening on %s', port);
+});
