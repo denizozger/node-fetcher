f7f4644990db464af2869a4c3ceaafda78b2a6a8
denizozger <deniz@parallelbrains.com>
2013-11-19 10:24:25 +0000

    Some minor amendments

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const resourceDefaultVersion = -1;
 const dataRequestingServerURL = process.env.DATA_REQUESTING_SERVER_URL || 'http://node-websocket-server.herokuapp.com/broadcast/';
 const dataProviderHost = process.env.DATA_PROVIDER_HOST || 'node-dataprovider.herokuapp.com';
 const dataProviderPort = process.env.DATA_PROVIDER_PORT || 80;
-const fetchingJobTimeoutInMilis = 10000;
+const fetchingJobTimeoutInMilis = 5000;
 var allDataFetched = true;
 const authorizationHeaderKey = 'bm9kZS13ZWJzb2NrZXQ=';
 const nodeWebSocketAuthorizationHeaderKey = 'bm9kZS1mZXRjaGVy';
@@ -40,33 +40,37 @@ fetchingJob();
 /**
  * Receive data fetch requests
  */
-app.get('/fetchlist/new/?*', function(request, response) {
+app.get('/fetchlist/new/?*', function(req, res) {
   // Security
-  if (request.header('Authorization') !== authorizationHeaderKey) {
-    var ip = request.ip || request.connection.remoteAddress || request.socket.remoteAddress || request.connection.socket.remoteAddress;
+  if (req.header('Authorization') !== authorizationHeaderKey) {
+    var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 
     console.warn('Unknown server (%s) tried to add a new key to fetch list', ip);
 
-    response.writeHead(403, {
+    res.writeHead(403, {
       'Content-Type': 'text/plain'
     }); 
-    response.shouldKeepAlive = false;
-    response.write('You are not allowed to get data from this server\n');
-    response.end();
+    res.shouldKeepAlive = false;
+    res.write('You are not allowed to get data from this server\n');
+    res.end();
     return;
   }
 
-  var requestedResourceId = request.params[0];
+  handleNewDataFetchRequests(req, res);
+});
+
+function handleNewDataFetchRequests(req, res) {
+  var requestedResourceId = req.params[0];
 
   if(!requestedResourceId) {
     console.warn('Bad Request: Invalid parameters');
-    response.statusCode = 400;
-    return response.send('Bad Request');
+    res.statusCode = 400;
+    return res.send('Bad Request');
   }
 
   if (resourcesAndVersions[requestedResourceId]) {
     console.warn('This resource information is in the fetchlist already. Resource id: %s', requestedResourceId);
-    return response.send('This resource information is in the fetchlist already');
+    return res.send('This resource information is in the fetchlist already');
   }
 
   resourcesAndVersions[requestedResourceId] = resourceDefaultVersion;
@@ -74,8 +78,8 @@ app.get('/fetchlist/new/?*', function(request, response) {
   console.log('Successfully added resource (id: %s) to the fetchlist. Current fetchlist:', requestedResourceId);
   console.log(JSON.stringify(resourcesAndVersions, null, 4));
 
-  response.send('Success');
-});
+  res.send('Success');  
+}
 
 app.get('/', function(req, res){
   var body = 'node-fetcher';
@@ -99,7 +103,6 @@ function fetchData() {
 
   // Asynchronously fetch resource data
   async.forEach(_.keys(resourcesAndVersions), function (resourceId, callback) { 
-
     options.path = '/' + resourceId;
 
     console.log('Fetching data for resource %s from %s:%s', resourceId, dataProviderHost, dataProviderPort);
@@ -114,64 +117,64 @@ function fetchData() {
         console.log('Received some data from data source: %s', updatedResourceInJSON);
       });
 
-      // When all data is received, check its version and broadcast it if received version is greater
-      res.on('end', function(){
-        var updatedResource;
-        try {
-          updatedResource = JSON.parse(updatedResourceInJSON);
-        } catch (e) {
-          console.error('Did not receive proper JSON object from data provider for resource %s', resourceId);
-        }
+        // When all data is received, check its version and broadcast it if received version is greater
+      res.on('end', function() {
+          var updatedResource;
+          try {
+            updatedResource = JSON.parse(updatedResourceInJSON);
+          } catch (e) {
+            console.error('Did not receive proper JSON object from data provider for resource %s', resourceId);
+          }
 
-        var existingVersion = resourcesAndVersions[resourceId];
-        var newVersion;
+          var existingVersion = resourcesAndVersions[resourceId];
+          var newVersion;
 
-        if (updatedResource) {
-          newVersion = updatedResource.version;
-        }
+          if (updatedResource) {
+            newVersion = updatedResource.version;
+          }
 
-        // Compare the existing and new versions, broadcast if necesary
-        if (updatedResource) {
-          // if there is verson information, compare the versions and broadcast if data is newer
-          if (isNumber(existingVersion) && isNumber(newVersion) && newVersion > existingVersion) {
-            console.log('Changes detected for resource %s, current version is %s, new version is %s', 
-              resourceId, existingVersion, newVersion);  
-            broadcastNewResourceData(updatedResource, resourceId);
-            resourcesAndVersions[resourceId] = newVersion; // update the version
-          } else if (isNumber(existingVersion) && isNumber(newVersion) && newVersion <= existingVersion){
-            console.log('No changes detected for resource %s, current version is %s, new version is %s', 
-              resourceId, existingVersion, newVersion);  
+          // Compare the existing and new versions, broadcast if necesary
+          if (updatedResource) {
+            // if there is verson information, compare the versions and broadcast if data is newer
+            if (isNumber(existingVersion) && isNumber(newVersion) && newVersion > existingVersion) {
+              console.log('Changes detected for resource %s, current version is %s, new version is %s', 
+                resourceId, existingVersion, newVersion);  
+              broadcastNewResourceData(updatedResource, resourceId);
+              resourcesAndVersions[resourceId] = newVersion; // update the version
+            } else if (isNumber(existingVersion) && isNumber(newVersion) && newVersion <= existingVersion){
+              console.log('No changes detected for resource %s, current version is %s, new version is %s', 
+                resourceId, existingVersion, newVersion);  
+            } else {
+              console.warn('No valid version information detected (current: %s, new: %s), broadcasting the data.',
+                existingVersion, newVersion);
+              broadcastNewResourceData(updatedResource, resourceId);
+            }
+
+            // if the resource is termianted, remove it from the list
+            if (updatedResource.terminated === true) {
+              console.log('Resource appears to be terminated, removing it from the list');
+              delete resourcesAndVersions[resourceId];
+            }
           } else {
-            console.warn('No valid version information detected (current: %s, new: %s), broadcasting the data.',
-              existingVersion, newVersion);
-            broadcastNewResourceData(updatedResource, resourceId);
+            console.error('Could not receive new resource data or it was corrupt');
           }
 
-          // if the resource is termianted, remove it from the list
-          if (updatedResource.terminated === true) {
-            console.log('Resource appears to be terminated, removing it from the list');
-            delete resourcesAndVersions[resourceId];
-          }
-        } else {
-          console.error('Could not receive new resource data or it was corrupt');
-        }
+          console.log('All data for resource %s has been received', resourceId);
+          callback();
+        });
 
-        console.log('All data for resource %s has been received', resourceId);
-        callback();
+        res.on('error', function(e) {
+          console.error('Can not fetch resource data: %s', e.message);
+        });
       });
-
-      res.on('error', function(e) {
-      console.error('Can not fetch resource data: %s', e.message);
-    });
-    });
-  }, function(err) {
-    if (err) {
-      console.error('Cant fetch resource data: %s', err);  
-    } 
-      
-    allDataFetched = true; 
-    console.log('[COMPLETE] Data fetch is complete');
-  }); 
+    }, function(err) {
+      if (err) {
+        console.error('Cant fetch resource data: %s', err);  
+      } 
+        
+      allDataFetched = true; 
+      console.log('[COMPLETE] Data fetch is complete');
+    }); 
 }
 
 // Send new resource data to websocket client - or any other server
